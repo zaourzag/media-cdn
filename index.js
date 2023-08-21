148ed65e245e35988fa906843127ae963efd8a03
Zakaria aourzag <z.aourzag@gmail.com>
2023-08-21 17:47:24 +0200

    new code who dis

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-app.listen(6060, () => {
-    console.log(`The application is running on localhost:6060!`);
+app.listen(27, () => {
+    console.log(`The application is running on localhost:27!`);
 });
 const { createLogger, transports, format, Logger,winston } = require("winston")
 const expressWinston = require('express-winston');
@@ -25,7 +25,7 @@ app.use(expressWinston.logger({
     meta: true, // optional: control whether you want to log the meta data about the request (default to true)
     msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
     expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
-    colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
     ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or responsezzzzzzz
   }));
 app.get('/api/v1/splashscreen.png', async (req, res) => {
